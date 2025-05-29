4cd4db6664203f6c1e843b1425dcce47cb74631e
firstmic79 <78557369+firstmic79@users.noreply.github.com>
2025-05-29 17:37:31 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,23 @@
-self.addEventListener('install', function(event) {
-  console.log('Service worker installed.');
-  self.skipWaiting();
+const cacheName = 'nosh-inventory-v1';
+const filesToCache = [
+  '/',
+  '/inventory-pwa/index.html',
+  '/inventory-pwa/manifest.json',
+  '/inventory-pwa/icon-192.png',
+  '/inventory-pwa/icon-512.png',
+  '/inventory-pwa/apple-touch-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('Service worker activated.');
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request).catch(() => new Response("Offline")));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
